18b3342571ca6922059e486edf23d44e0bcfa456
MoyClub Admin <admin@oliveoil-moyclub.systems>
2023-11-12 14:20:18 +0000

    Text Changes Made

--- resources/views/oil/create.blade.php
+++ resources/views/oil/create.blade.php
@@ -22,7 +22,7 @@
         <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
           <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
             <div class="text-gray-600">
-              <p class="font-medium text-lg">تأكيد طلب تنكة زيت</p>
+              <p class="font-medium text-lg">تأكيد طلب العدد المطلوب</p>
               <p>الرجاء تعبئة النموذج الاتي كاملا.</p>
             </div>
 
